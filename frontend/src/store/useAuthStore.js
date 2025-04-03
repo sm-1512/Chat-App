ecf41fbf1452bbac4a020afb0f25537aa31f5b9b
Sahil Mishra <sahil.mishra1512@gmail.com>
2025-04-03 06:47:21 +0530

    Real Time Chat App

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -59,5 +59,18 @@ export const useAuthStore = create((set, get) => ({
     }
   },
 
-  updateProfile : async(data) => {},
+  updateProfile : async(data) => {
+    set({isUpdatingProfile:true});
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      toast.success("Profile updated successfully");
+      set({authUser: res.data})
+    } catch (error) {
+      console.log("error in update profile", error);
+      toast.error(error.response.data.message);
+    }
+    finally{
+      set({isUpdatingProfile:false});
+    }
+  },
 }));
