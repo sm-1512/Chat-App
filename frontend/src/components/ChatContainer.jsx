2707a5756d72dd9e99dfc5584abe56455abb5c11
Sahil Mishra <sahil.mishra1512@gmail.com>
2025-04-04 05:50:41 +0530

    Real Time Chat App

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import { useChatStore } from '../store/useChatStore'
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
-  const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore();
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
 
-  if(isMessagesLoading) return <div>Loading...</div>
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+
   return (
-    <div>ChatContainer</div>
-  )
-}
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
 
-export default ChatContainer
+      <p>messages...</p>
+
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
